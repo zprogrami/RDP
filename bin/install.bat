0e8b2441bc7232e20541f425b2fb4e177870450c
sebaxakerhtc <32651506+sebaxakerhtc@users.noreply.github.com>
2021-11-29 00:54:24 +0200

    Update install.bat

--- bin/install.bat
+++ bin/install.bat
@@ -4,6 +4,7 @@ if not exist "%~dp0RDPWInst.exe" goto :error
 xcopy "%~dp0*" "C:\Program Files\RDP Wrapper\"  /s /I /y
 ping -n 3 localhost > nul
 cmd.exe /C start "" "C:\Program Files\RDP Wrapper\RDP_CnC.exe"
+exit
 :error
 echo [-] Installer executable not found.
 echo Please extract all files from the downloaded package or check your anti-virus.
